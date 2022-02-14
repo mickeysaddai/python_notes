fcf6e19f9fb300bd20aae0a505e70567ddb38aad
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2022-02-13 22:05:16 -0500

    first commit

--- 1.basics.py
+++ 1.basics.py
@@ -1,4 +1,10 @@
-# pylint: disable=missing-module-docstring
+# error handling
+a = 5
+try:
+    print(len(a))
+except:
+    print(f'{a} has no length')
+
 # def divide(x, y):
 #     try:
 #         result = x / y
@@ -11,16 +17,11 @@
 # print(divide(10, 5))
 
 
-# def greeting():
-#     try:
-#         return "Hey, friend."
-#     finally:
-#         return "Fun times!"
+def greeting():
+    try:
+        return "Hey, friend."
+    finally:
+        return "Fun times!"
 
 
-# print(greeting())
-a = True
-try:
-    print(len(a))
-except:
-    print(f'{a} has no length')
+print(greeting())
