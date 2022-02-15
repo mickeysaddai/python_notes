6dcfcd6b6ff950f4950154e39094ac7bb64d42ee
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2022-02-15 15:48:57 -0500

    input validation

--- 12.input_validation.py
+++ 12.input_validation.py
@@ -4,18 +4,21 @@
 #-make it a number
 #-handle exceptions
 #-require valid input
-age = 1
-while age:
-    age = input("What's your age? ")
+# age = 1
+# while age:
+#     age = input("What's your age? ")
 
-    if age:
-        try:
-            age = int(float(age))
-            if age > 0 and age < 120:
-                print(f"Cool! You are {age} years old")
-            else:
-                print('Out of range. Please try again')
-        except:
-            print("Please enter a number")
+#     if age:
+#         try:
+#             age = int(float(age))
+#             if age > 0 and age < 120:
+#                 print(f"Cool! You are {age} years old")
+#             else:
+#                 print('Out of range. Please try again')
+#         except:
+#             print("Please enter a number")
 
-
+nums = set([1, 2, 3, 4, 5, 4, 3, 2, 1])
+print(sum(nums))
+nums = ['1', '2', '3', '4', '5', '4', '3', '2', '1']
+print(sum(nums))
